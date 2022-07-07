e8ce52477ea03ccf51d7b38f4c3933e76571f99a
Airam Toscano <airamlobato@gmail.com>
2022-07-06 21:40:01 -0400

    Feat: migrations matches

---
+++ app/backend/src/database/migrations/20220706224028-matches.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const MatchesTable = queryInterface.createTable("matches", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+
+    return MatchesTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable("matches"),
+};
