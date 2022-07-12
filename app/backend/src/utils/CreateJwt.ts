5f99d4f95f9a2c1088708bfb2f6d077ede6ce109
Airam Toscano <airamlobato@gmail.com>
2022-07-11 20:40:17 -0400

    Feat: validJwt

--- app/backend/src/utils/CreateJwt.ts
+++ app/backend/src/utils/CreateJwt.ts
@@ -1,5 +1,5 @@
-import { sign, SignOptions } from 'jsonwebtoken';
-import { IModel } from '../interfaces/LoginInterfaces';
+import { sign, SignOptions, decode } from 'jsonwebtoken';
+import { IModel, JwtPayloadHandler } from '../interfaces/LoginInterfaces';
 
 export default class CreateJWT {
   constructor(private model: IModel) {
@@ -8,7 +8,7 @@ export default class CreateJWT {
 
   async createJwt(email: string) {
     const listUser = await this.model.findOne(email);
-    const secret = String(process.env.JWT_SECRET)
+    const secret = String(process.env.JWT_SECRET);
     const signInOptions: SignOptions = {
       algorithm: 'HS256',
       expiresIn: '1h',
@@ -17,5 +17,15 @@ export default class CreateJWT {
       name: listUser.email,
     };
     return sign(payload, secret, signInOptions);
+  }
+
+  async validJwt(token: string) {
+    const validToken = decode(token);
+    if (validToken != null) {
+      const { name } = validToken as JwtPayloadHandler;
+      const listUser = await this.model.findOne(name);
+      return listUser.role;
+    }
+    return false;
   }
 }
