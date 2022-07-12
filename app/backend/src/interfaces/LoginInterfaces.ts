0414ab56edcc844df92ec99e6148e37f8e1d7e89
Airam Toscano <airamlobato@gmail.com>
2022-07-11 20:37:26 -0400

    Feat: JwtPayloadHandler and Ijwt

--- app/backend/src/interfaces/LoginInterfaces.ts
+++ app/backend/src/interfaces/LoginInterfaces.ts
@@ -1,3 +1,4 @@
+import { JwtPayload } from 'jsonwebtoken';
 import Users from '../database/models/Users';
 
 export interface IModel {
@@ -10,4 +11,9 @@ export interface Iservice {
 
 export interface Ijwt {
   createJwt(email: string):Promise<string>
+  validJwt(token: string | undefined):Promise<string | boolean>
+}
+
+export interface JwtPayloadHandler extends JwtPayload {
+  name: string;
 }
