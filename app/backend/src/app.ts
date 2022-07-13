ebe4c661eb1c2e989b5ffabaac1d424076ee7d82
Airam Toscano <airamlobato@gmail.com>
2022-07-12 21:10:14 -0400

    Modified: app.ts

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import LoginRoute from './routes/LoginRoutes';
+import TeamsRoute from './routes/TeamsRoutes';
 
 class App {
   public app: express.Express;
@@ -23,7 +24,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
-    this.app.use(LoginRoute);
+    this.app.use(LoginRoute, TeamsRoute);
   }
 
   public start(PORT: string | number):void {
