90d5e3a239aaa5ba390cc746af12c5da2f2cec64
Airam Toscano <airamlobato@gmail.com>
2022-07-13 01:11:50 -0400

    Modified: Matches interfaces

--- app/backend/src/interfaces/Matchesnterfaces.ts
+++
@@ -1,9 +0,0 @@
-import Matches from '../database/models/Matches';
-
-export interface IModelMatches {
-  findMatches():Promise<Matches[]>
-}
-
-export interface IserviceMatches {
-  findMatches():Promise<Matches[]>
-}
